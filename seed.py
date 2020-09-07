60c3cd79f089700ecfa50f797a4a83ff4fccac84
MANTHAN JINDAL <manthanj@cse.iitb.ac.in>
2020-09-07 17:27:27 +0530

    Update seed.py
    basic functionalities

--- seed.py
+++ seed.py
@@ -1,34 +1,35 @@
 from socket import *
-import thread
+from _thread import *
+import json
 
 ip = input()
 port = int(input())
 
 s = socket(AF_INET, SOCK_STREAM)
-s.bind(ip, port)
+s.bind((ip, port))
 s.listen(10)
 pl = []
-
+# output_file = 
 
 def on_new_client(clientsocket,addr):
-    msg = #something containing pl
-    clientsocket.send(msg)
+    l_addr = clientsocket.recv(1024)
+    msg = json.dumps(pl)#something containing pl
+    clientsocket.send(msg.encode())
+    pl.append(l_addr)
     while addr in pl:
-        msg = clientsocket.recv(#size of dead message)
-        #do some checks and if msg == someWeirdSignal: break:
-    #     print addr, ' >> ', msg
-    #     msg = raw_input('SERVER >> ')
-    #     #Maybe some code to compute the last digit of PI, play game or anything else can go here and when you are done.
-    #     clientsocket.send(msg)
-    # clientsocket.close()
+        msg = clientsocket.recv(1024)#more than size of dead message format
+        msg = msg.decode()
         m = msg.split(":")
         assert(m[0] == "Dead Node")
-        if (m[1], m[2]) in pl:
-            pl.remove((m[1], m[2]))
+        if (m[1], int(m[2])) in pl:
+            pl.remove((m[1],int(m[2])))
     clientsocket.close()
 
 
 while(True):
+    print("Send Client")
     c, addr = s.accept()
-    pl.push_back((addr))
-    thread.start_new_thread(on_new_client,(c,addr))
+    # pl.append(addr)
+    start_new_thread(on_new_client,(c,addr))
+
+s.close()
